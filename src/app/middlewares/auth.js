f00252e0335eefc700b0d398fb7ea41e1b4d2ebe
Arthur Junio <arthu_junior@hotmail.com>
2019-10-25 18:10:46 -0300

    Validations schemas com Yup

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -6,18 +6,17 @@ export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
-    res.status(401).json({ error: 'Token Not Provided ' });
+    return res.status(401).json({ error: 'Token not provided' });
   }
 
   const [, token] = authHeader.split(' ');
+
   try {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
     req.userId = decoded.id;
-
     return next();
-  } catch (err) {
-    res.status(401).json({ error: 'Invalid Token' });
+  } catch (error) {
+    return res.status(401).json({ error: 'Token invalid' });
   }
-
-  return next();
 };
